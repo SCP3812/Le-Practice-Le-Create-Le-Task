7b74480b4496a0d209a9d125e9eadb94c0a66a25
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2023-09-13 21:23:34 +0000

    deleted some things

--- practice-create-task.py
+++ practice-create-task.py
@@ -6,24 +6,20 @@ def Markovian(markov):
     markov_chain = {}
     markov_list = markov.split(" ")
 
-    n = 0
     for items in markov_list:
-        if not markov_chain(items):
-            markov_chain[items] = []
-        if markov_list[n] != markov_list[n+1]:
-            markov_chain[items].append(items)
-        n = n + 1
+        markov_chain[items] = []
+        markov_chain[items].append(items)
     words = markov_chain.keys()
-    word = words[randInt(0, len(words))]
+    word = words[random.randint(0, len(words))]
     result = ""
 
     for i in len(words):
         result = word + " "
-        newWord = markov_chain[word][randInt(0, len(words))]
+        newWord = markov_chain[word][random.randint(0, len(words))]
         word = newWord
 
         if not word or not word in markov_chain:
-            word = words[randInt(0, len(words))]
+            word = words[random.randint(0, len(words))]
     return result
 
 
